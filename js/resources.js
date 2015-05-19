49c388c3c95163c2e4c401cfb102f956a9c79b0b
joshua inocencio <snotbot1234@gmai.com>
2015-05-19 13:58:42 -0700

    commit

--- js/resources.js
+++ js/resources.js
@@ -4,18 +4,21 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
+        {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+        {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+        {name: "player-1", type:"image", src: "data/img/player-1.png"},
+        
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
-		
+	
 	/* Maps. 
 	 * @example
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+        {name: "level01", type: "tmx", src: "data/map/lvl1.tmx"},
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
